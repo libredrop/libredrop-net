a782ba4c7a4e312e240e15db71f3d28833589f17
Povilas Balciunas <balciunas90@gmail.com>
2018-11-14 21:19:25 +0200

    Add peer discovery examples

---
+++ examples/peer_discovery.rs
@@ -0,0 +1,28 @@
+//! Demontrates how to discover peers on LAN.
+
+extern crate env_logger;
+extern crate libredrop_net;
+#[macro_use]
+extern crate log;
+extern crate futures;
+extern crate tokio;
+#[macro_use]
+extern crate unwrap;
+
+use futures::Stream;
+use libredrop_net::discover_peers;
+use tokio::runtime::current_thread::Runtime;
+
+fn main() {
+    env_logger::init();
+    let mut evloop = unwrap!(Runtime::new());
+
+    info!("Looking for peers on LAN on port 6000");
+    let find_peers = discover_peers(6000)
+        .map_err(|e| error!("Peer discovery failed: {:?}", e))
+        .for_each(|addrs| {
+            println!("Peer is listening on: {:?}", addrs);
+            Ok(())
+        });
+    unwrap!(evloop.block_on(find_peers));
+}
---
+++ examples/peer_discovery_server.rs
@@ -0,0 +1,50 @@
+//! Demontrates how to setup peer discovery server.
+//! This server will respond to whoever is looking for peers on LAN.
+
+extern crate get_if_addrs;
+extern crate libredrop_net;
+#[macro_use]
+extern crate unwrap;
+extern crate env_logger;
+extern crate future_utils;
+extern crate tokio;
+#[macro_use]
+extern crate log;
+
+use future_utils::FutureExt;
+use get_if_addrs::{get_if_addrs, IfAddr};
+use libredrop_net::DiscoveryServer;
+use std::io;
+use std::net::{SocketAddr, SocketAddrV4};
+use tokio::prelude::Future;
+
+fn main() -> io::Result<()> {
+    env_logger::init();
+
+    info!("Starting peer discovery server on port 6000");
+    let addrs = our_addrs(1234)?;
+    info!("Our advertised addresses: {:?}", addrs);
+
+    let server = unwrap!(DiscoveryServer::new(6000, addrs));
+    let run_server = server
+        .map(|_| ())
+        .log_error(log::LogLevel::Error, "Peer discovery server failure")
+        .map_err(|_| ());
+    tokio::run(run_server);
+
+    Ok(())
+}
+
+/// Construct a list of fake listening addresses.
+fn our_addrs(with_port: u16) -> io::Result<Vec<SocketAddr>> {
+    let interfaces = get_if_addrs()?;
+    let addrs = interfaces
+        .iter()
+        .filter_map(|interface| match interface.addr {
+            IfAddr::V4(ref ifv4_addr) => Some(ifv4_addr.ip),
+            IfAddr::V6(_) => None,
+        }).filter(|ip| !ip.is_loopback())
+        .map(|ip| SocketAddr::V4(SocketAddrV4::new(ip, with_port)))
+        .collect();
+    Ok(addrs)
+}
